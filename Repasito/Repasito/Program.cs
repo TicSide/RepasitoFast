2ce537ab8ddf5d33da8158d19a7fcd530e00d6db
Omar Harold Rivera Ticllacuri <66962537+TicSide@users.noreply.github.com>
2026-05-29 22:42:59 -0500

    feat(proof): Add Some Innecesary code

---
+++ Repasito/Repasito/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repasito
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string abd = "XD";
+            int a;
+            Console.WriteLine("mira lo que dijo este: {0}", abd);
+            a = Convert.ToInt32(Console.ReadLine());
+
+            int gg= Convert.ToInt32(Console.ReadLine());
+
+            string cd = Console.ReadLine();              Console.WriteLine("mira lo que dijo este: {0}", cd);
+             Console.WriteLine("mira lo que dijo este: {0}", gg);
+             Console.WriteLine("mira lo que dijo este: {0}", a);
+
+            String pipapu = Console.ReadLine();
+
+
+
+        }
+    }
+}
